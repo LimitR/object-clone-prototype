0a9191dbc02f691327d2bfb3757a01e24871a003
LimitR <kakashihotakimail.ru@gmail.com>
2021-11-16 15:04:44 +0300

    array and function

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-module.exports = (function clone (){Object.prototype.clone = function clone(writable = true){
-    let new_object = {}
-    for (const [key, value] of Object.entries(this)) {
-        new_object[key] = {value: value, enumerable: true, writable: writable ? true : false}
+module.exports = (
+    Object.prototype.clone = function clone(writable = true){
+        if (Array.isArray(this)){
+            return Array.from(this)
+        }
+        if (typeof this === 'function'){
+            return this.bind(this)
+        }
+        if (typeof this === 'object'){
+            let new_object = {}
+            for (const [key, value] of Object.entries(this)) {
+                new_object[key] = {value: value, enumerable: true, writable: writable ? true : false}
+            }
+            return Object.create(Object.getPrototypeOf(this),  new_object )
+        }
+
     }
-    return Object.create(Object.getPrototypeOf(this),  new_object )
-}})()
+)
