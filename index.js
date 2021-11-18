5616e2e467aee4d3fba599a3cafa07a489996fb9
LimitR <kakashihotakimail.ru@gmail.com>
2021-11-18 08:25:27 +0300

    fix return value

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 'use strict'
 module.exports = (
-    /**
-     * @param {Boolean} [writable]
-     */
-        (object)=> {
-        Object.defineProperty(object, 'clone', {
-            value: function (writable = true) {
-                if (Array.isArray(this)) {
-                    return Array.from(this)
-                }
-                if (typeof this === 'function') {
-                    return this.bind(this)
-                }
-                if (typeof this === 'object') {
-                    if (writable === true) {
-                        return Object.create(Object.getPrototypeOf(this), Object.getOwnPropertyDescriptors(this))
-                    } else {
-                        return Object.create(Object.getPrototypeOf(this), Object.entries(this).reduce((accumulator, [key, value]) => {
-                            accumulator[key] = {
-                                value: value,
-                                enumerable: true,
-                                writable: false
-                            }
-                            return accumulator
-                        }, {}))
+    function () {
+        Array.from(arguments).forEach((element)=>{
+            Object.defineProperty(element, 'clone', {
+                value: function (writable = true) {
+                    if (Array.isArray(this)) {
+                        return Array.from(this)
                     }
-                }
-            },
-            writable: false,
-            enumerable: false
+                    if (typeof this === 'function') {
+                        return this.bind(this)
+                    }
+                    if (typeof this === 'object') {
+                        if (writable === true) {
+                            return Object.create(Object.getPrototypeOf(this), Object.getOwnPropertyDescriptors(this))
+                        } else {
+                            return Object.create(Object.getPrototypeOf(this), Object.entries(this).reduce((accumulator, [key, value]) => {
+                                accumulator[key] = {
+                                    value: value,
+                                    enumerable: true,
+                                    writable: false
+                                }
+                                return accumulator
+                            }, {}))
+                        }
+                    }
+                },
+                writable: false,
+                enumerable: false
+            })
         })
-        return object.clone()
+        return arguments[0].clone(this)
     }
 )
