6d7e1c0b535bb54cde809db157f5cc784b6f9e80
LimitR <kakashihotakimail.ru@gmail.com>
2021-11-18 07:04:00 +0300

    return func

--- index.js
+++ index.js
@@ -3,26 +3,32 @@ module.exports = (
     /**
      * @param {Boolean} [writable]
      */
-    Object.defineProperty(Object.prototype, 'clone',{
-        value: function (writable = true) {
-            if (Array.isArray(this)){
-                return Array.from(this)
-            }
-            if (typeof this === 'function'){
-                return this.bind(this)
-            }
-            if (typeof this === 'object'){
-                if(writable === true ){
-                    return Object.create(Object.getPrototypeOf(this), Object.getOwnPropertyDescriptors(this) )
-                }else {
-                    return Object.create(Object.getPrototypeOf(this), Object.entries(this).reduce((accumulator, [key, value]) => {
-                        accumulator[key] = {value: value, enumerable: true, writable: writable === true? true : false}
-                        return accumulator
-                    }, {}))
+        (object)=> {
+        Object.defineProperty(object, 'clone', {
+            value: function (writable = true) {
+                if (Array.isArray(this)) {
+                    return Array.from(this)
                 }
-            }
-        },
-        writable: false,
-        enumerable: false
-    })
+                if (typeof this === 'function') {
+                    return this.bind(this)
+                }
+                if (typeof this === 'object') {
+                    if (writable === true) {
+                        return Object.create(Object.getPrototypeOf(this), Object.getOwnPropertyDescriptors(this))
+                    } else {
+                        return Object.create(Object.getPrototypeOf(this), Object.entries(this).reduce((accumulator, [key, value]) => {
+                            accumulator[key] = {
+                                value: value,
+                                enumerable: true,
+                                writable: false
+                            }
+                            return accumulator
+                        }, {}))
+                    }
+                }
+            },
+            writable: false,
+            enumerable: false
+        })
+    }
 )
