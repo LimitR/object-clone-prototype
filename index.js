2f58442620fdd177e2f9860632e5eeb3cf981c7e
LimitR <kakashihotakimail.ru@gmail.com>
2021-11-19 08:03:20 +0300

    fix clone array

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ module.exports = (
                     }
                 },
                 writable: true,
-                enumerable: false
+                enumerable: false,
+                configurable: true
             })
         })
         return arguments[0].clone(this)
@@ -17,14 +18,20 @@ module.exports = (
 )
 
 function chekObject(object_value, writable){
-    return Object.create(Object.prototype, Object.entries(object_value).reduce((accumulator, [key, value])=>{
+    return Object.create(object_value.__proto__, Object.entries(object_value).reduce((accumulator, [key, value])=>{
         if(Array.isArray(value)){
-            if(value.filter((element)=> typeof element === 'object').length > 0
-                &&
-                value.filter((element)=> !Array.isArray(element))
-            ){
+            if(value.filter((element)=> typeof element === 'object').length > 0){
                 accumulator[key] = {
-                    value: [value.filter((element)=> typeof element != 'object'), chekObject(value.filter((element)=> typeof element === 'object'), writable)[0]],
+                    value: [value.filter((element)=> typeof element != 'object'), chekObject(value.filter((element)=> typeof element === 'object'), writable)]
+                        .flat()
+                        .map((elment)=>{
+                            if(typeof elment === 'object'){
+                                return  Object.values(elment)
+                            }else {
+                                return elment
+                            }
+
+                        }).flat(),
                     enumerable: true,
                     writable: writable
                 }
